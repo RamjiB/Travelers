3cbeb51e2b8f04d6724eb9d88ec332ba36ba85e4
RamjiB <ramji.b28@gmail.com>
2018-02-26 08:53:31 +0530

    push after first review

---
+++ app/src/main/java/ramji/travelers/AppWidgetProvider.java
@@ -0,0 +1,67 @@
+package ramji.travelers;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import java.util.ArrayList;
+
+import ramji.travelers.image_details.ImageDetailsView;
+
+
+public class AppWidgetProvider extends android.appwidget.AppWidgetProvider {
+
+    private static final String TAG = "AppWidgetProvider";
+
+    public static final String UPDATE_MEETING_ACTION = "android.appwidget.action.APPWIDGET_UPDATE";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        Log.i(TAG,"widget onUpdate");
+        FavouritePlaceUpdatedWidget.startActionUpdateFavWidgets(context);
+
+    }
+
+    public static void updateAppWidget(Context context,AppWidgetManager appWidgetManager,
+                                       ArrayList<String> location,int[] appWidgetIds) {
+        Log.i(TAG,"widget updateAppWidget");
+        // update each of the app widgets with the remote adapter
+        for (int appWidgetId : appWidgetIds) {
+            Log.i(TAG,"location size: "+ location.size());
+            updateAppFavWidget(context,appWidgetManager,location,appWidgetId);
+        }
+
+    }
+
+    private static void updateAppFavWidget(Context context, AppWidgetManager appWidgetManager,
+                                           ArrayList<String> location,int appWidgetId) {
+
+        Log.i(TAG,"widget updateAppFavWidget");
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.appwidget);
+
+        Intent intent = new Intent(context,HomeActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
+        Log.i(TAG,"location size: "+ location.size());
+        String location_list  = "";
+        for (int i = 0; i < location.size(); i++){
+            int position = i+1;
+            Log.i(TAG,"updateAppFavWidget: " + location.get(i));
+            location_list = location_list + position  +". " + location.get(i) +" \n";
+            remoteViews.setTextViewText(R.id.text_View,location_list);
+        }
+        Log.i(TAG,"location_list: "+ location.toString());
+        remoteViews.setOnClickPendingIntent(R.id.locationText,pendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetId,remoteViews);
+    }
+
+
+
+}
---
+++ app/src/main/java/ramji/travelers/FavouritePlaceUpdatedWidget.java
@@ -0,0 +1,97 @@
+package ramji.travelers;
+
+
+import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class FavouritePlaceUpdatedWidget extends IntentService{
+
+    private static final String TAG = "FavouritePlaceUpdatedWi";
+
+    private static final String ACTION_UPDATE_RECIPE_WIDGETS = "ramji.travelers.update_fav_widgets";
+
+
+    public FavouritePlaceUpdatedWidget() {
+        super("FavouritePlaceUpdatedWidget");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.i(TAG,"widget onHandleIntent");
+        final String action = intent.getAction();
+        if (ACTION_UPDATE_RECIPE_WIDGETS.equals(action))
+            handleActionUpdateWidgets();
+
+    }
+
+    private void handleActionUpdateWidgets() {
+        Log.i(TAG, "widget: handleActionUpdateWidgets ");
+
+        final ArrayList<String> location = new ArrayList<>();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+            Query query = databaseReference.child(getString(R.string.dbname_saved_photos))
+                    .child(userId);
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
+                        location.add(singleSnapshot
+                                .child(getString(R.string.db_location)).getValue().toString());
+                        Log.i(TAG,"locations: "+location);
+
+                        AppWidgetManager appWidgetManager = AppWidgetManager
+                                .getInstance(getApplicationContext());
+                        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                                new ComponentName(getApplication(),AppWidgetProvider.class));
+                        Log.i(TAG,"location size: "+ location.size());
+                        AppWidgetProvider.updateAppWidget(getApplicationContext()
+                                ,appWidgetManager,location,appWidgetIds);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }else{
+            Log.i(TAG,"locations: "+location);
+            location.add(getResources().getString(R.string.widget_empty_string) + " \n" +
+                            "Please sign in");
+            AppWidgetManager appWidgetManager = AppWidgetManager
+                    .getInstance(getApplicationContext());
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                    new ComponentName(getApplication(),AppWidgetProvider.class));
+            Log.i(TAG,"location size: "+ location.size());
+            AppWidgetProvider.updateAppWidget(this,appWidgetManager,location,appWidgetIds);
+        }
+
+
+    }
+
+    public static void startActionUpdateFavWidgets(Context context){
+        Intent intent = new Intent(context,FavouritePlaceUpdatedWidget.class);
+        intent.setAction(ACTION_UPDATE_RECIPE_WIDGETS);
+        context.startService(intent);
+    }
+
+}
